cd24f03559c8e7dc9a2790b910f3249b06251ef3
Ross McKillop <rossmck@amazon.com>
2020-07-22 20:15:05 +0800

    initial commit

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePost = /* GraphQL */ `
+  subscription OnCreatePost {
+    onCreatePost {
+      id
+      name
+      location
+      description
+      image
+    }
+  }
+`;
+export const onUpdatePost = /* GraphQL */ `
+  subscription OnUpdatePost {
+    onUpdatePost {
+      id
+      name
+      location
+      description
+      image
+    }
+  }
+`;
+export const onDeletePost = /* GraphQL */ `
+  subscription OnDeletePost {
+    onDeletePost {
+      id
+      name
+      location
+      description
+      image
+    }
+  }
+`;
